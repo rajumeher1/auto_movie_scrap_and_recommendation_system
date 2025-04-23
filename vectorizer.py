a1f782136fdcbf1ef82780a25f8d60b9fedd816f
Raju Meher <rajumeher@Rajus-MacBook-Air.local>
2025-04-23 15:07:56 +0530

    update vectorizer.py

--- vectorizer.py
+++ vectorizer.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def vectorize_tags(data):
-    cv = CountVectorizer(max_features=2000)
+    cv = CountVectorizer(max_features=1000)
     vectors = cv.fit_transform(data)
     return vectors
 
@@ -11,7 +11,7 @@ def compute_similarity(vectors):
     top_10_similarity =[]
     for i in range(len(cs)):
         similarity = list(enumerate(cs[i]))
-        similarity = sorted(similarity, key=lambda x: x[1])[1:11]
+        similarity = sorted(similarity, key=lambda x: x[1], reverse=True)[1:11]
         top_10_similarity.append([i[0] for i in similarity])
     
     return top_10_similarity
